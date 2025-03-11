ea1425fadf7e818691316f609e0f1414bacb838c
jefersonbrendo <jefersonbrendo077@gmail.com>
2025-03-11 00:16:53 -0300

    modularizar funcionalidades

--- index.js
+++ index.js
@@ -1,72 +1,18 @@
-const inputItem = document.getElementById("input-item");
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+import verificarListaVazia from "./scripts/verificarListaVazia.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
-let contador = 0;
-
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
-
-function verificarListaVazia() {
-    const itensDaLista = listaDeCompras.querySelectorAll("li");
-    if (itensDaLista.length === 0) {
-        mensagemListaVazia.style.display = "block";
-    } else {
-        mensagemListaVazia.style.display = "none";
-    }
-}
-
 
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    if (inputItem.value === "") {
-        alert("Por favor, insira um item!");
-        return;
-    }
-
-    const itemDalista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-inputCheckbox.addEventListener("click", () => {
-    if (inputCheckbox.checked) {
-        nomeItem.style.textDecoration = "line-through";
-    } else {
-        nomeItem.style.textDecoration = "none";
-    }
-    
-})
-    
-    
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-    itemDalista.appendChild(containerItemDaLista);
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-br", {
-        weekday: "long"
-    });
-    const data = new Date().toLocaleDateString("pt-br");
-    const hora = new Date().toLocaleTimeString("pt-br", {
-        hour: "numeric",
-        minute: "numeric"
-    });
-    const dataCompleta = `${diaDaSemana} (${data}) ás ${hora}`;
-    const dataDoItem = document.createElement("P");
-    dataDoItem.classList.add("texto-data");
-    dataDoItem.innerText = dataCompleta;
-    itemDalista.appendChild(dataDoItem);
-
-
+    const itemDalista = criarItemDaLista(listaDeCompras);
     listaDeCompras.appendChild(itemDalista);
 
-    verificarListaVazia();
-
-    inputItem.value = '';
+    verificarListaVazia(listaDeCompras);
 })
+
+
+verificarListaVazia(listaDeCompras);
 
 
